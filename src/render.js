19a97b9e85ef0232efce25754c4a42eeb72f14e7
Jeff Yates <jeff@somewhatabstract.com>
2019-01-08 15:12:38 -0600

    Split out Apollo Network configuration
    Summary: This is just a small refactor. Since we're going to be changing the render file I wanted to make it a little easier to follow, so this clearly distinct functionality has been moved to its own file. I have not added additional tests as yet, but we could if we wanted to.
    
    Test Plan: `npm test`
    
    Reviewers: john, joshuan
    
    Differential Revision: https://phabricator.khanacademy.org/D51216

--- src/render.js
+++ src/render.js
@@ -13,20 +13,11 @@ const vm = require("vm");
 // TODO(csilvers): try to get rid of the dependency on jsdom
 const jsdom = require("jsdom");
 const ReactDOMServer = require('react-dom/server');
-const ApolloClient = require("apollo-client");
-const ReactApollo = require("react-apollo");
-const apolloCacheInmemory = require("apollo-cache-inmemory");
-const apolloLinkHttp = require("apollo-link-http");
-const fetch = require('node-fetch');
+const configureApolloNetwork = require('./configure-apollo-network.js');
 const raf = require("raf");
 
 const cache = require("./cache.js");
 const profile = require("./profile.js");
-
-const createHttpLink = apolloLinkHttp.createHttpLink;
-const InMemoryCache = apolloCacheInmemory.InMemoryCache;
-
-const BAD_URL = "BAD_URL";
 
 // render takes a cacheBehavior property, which is one of these:
 //    'yes': try to retrieve the object from the cache
@@ -184,81 +175,6 @@ const getVMContext = function(jsPackages, pathToReactComponent,
     return context;
 };
 
-// If Apollo network settings were provided then we need to expose some
-// important objects to the vm context. ApolloNetwork should have two
-// properties: 'url' (for the URL of the GraphQL endpoint) and 'headers'
-// for a key-value map of headers to send to the GraphQL endpoint. This
-// can include things such as cookies and xsrf tokens.
-// Requests will automatically timeout after 1000ms, unless another
-// timeout is provided via a 'timeout' property.
-const handleApolloNetwork = function(context) {
-    const handleNetworkFetch = (url, params) => {
-        if (!url || url === BAD_URL) {
-            return Promise.reject(
-                new Error("ApolloNetwork must have a valid url."));
-        }
-        return new Promise((resolve, reject) => {
-            let complete = false;
-
-            fetch(url, params)
-            .then(result => {
-                // Ignore requests that've already been aborted
-                // (this should never happen)
-                if (complete) {
-                    return;
-                }
-
-                // Handle server errors
-                if (result.status !== 200) {
-                    return reject(
-                        new Error("Server returned an error."));
-                }
-
-                complete = true;
-
-                resolve(result);
-            })
-            .catch(err => {
-                reject(err);
-            });
-
-            // After a specified timeout we abort the request if
-            // it's still on-going.
-            setTimeout(() => {
-                if (!complete) {
-                    complete = true;
-                    reject(new Error(
-                        "Server response exceeded timeout."));
-                }
-            }, context.ApolloNetwork.timeout || 1000);
-        });
-    };
-
-    Object.assign(context, {
-        // Specifically we need to use the server-side Node.js versions of
-        // apollo-client and react-apollo (the ones we use on the main site
-        // don't include the server-side rendering logic).
-        ApolloClient: ApolloClient,
-        ReactApollo: ReactApollo,
-
-        // Additionally, we need to build a request mechanism for actually
-        // making a network request to our GraphQL endpoint. We use the
-        // node-fetch module for making this request. This logic
-        // should be very similar to the logic held in apollo-wrapper.jsx.
-
-        ApolloNetworkLink: createHttpLink({
-            // HACK(briang): If you give the uri undefined, it will call
-            // fetch("/graphql") but we want to ensure that an undefined URL
-            // will fail the request.
-            uri: context.ApolloNetwork.url || BAD_URL,
-            fetch: handleNetworkFetch,
-            headers: context.ApolloNetwork.headers,
-        }),
-
-        ApolloCache: new InMemoryCache(),
-    });
-};
-
 /**
  * Actually render a React component.
  *
@@ -313,7 +229,7 @@ const render = function(jsPackages, pathToReactComponent, props,
     }
 
     if (context.ApolloNetwork) {
-        handleApolloNetwork(context);
+        configureApolloNetwork(context);
     }
 
     // getVMContext sets up the sandbox to have React installed, as
