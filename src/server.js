890dd0bf92c0b22c9ad604cad7148e601ad1b496
John Resig <jeresig@gmail.com>
2019-04-15 17:43:35 -0400

    Exclude CSS files from being downloaded by the SSR server
    Summary: These files aren't needed to render a page and downloading them will only cause problems. I excluded the CSS files from the downloads and also added tests to verify that they were working correctly.
    
    Test Plan: I ran `npm test` and it passed.
    
    Reviewers: #frontend-infrastructure, jeff
    
    Reviewed By: #frontend-infrastructure, jeff
    
    Differential Revision: https://phabricator.khanacademy.org/D53503

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
  * The high-level logic for our serving endpoints (api routes).
  */
 
-'use strict';
+"use strict";
 
 const bodyParser = require("body-parser");
 const express = require("express");
@@ -15,14 +15,12 @@ const profile = require("./profile.js");
 const renderSecret = require("./secret.js");
 const render = require("./render.js");
 
-
 // We keep track of how many render requests are currently "in
 // flight", to help us estimate how long a new request will take.
 let pendingRenderRequests = 0;
 
 const app = express();
-app.use(bodyParser.json({limit: '5mb'}));
-
+app.use(bodyParser.json({limit: "5mb"}));
 
 /**
  * Server-side render a react component.
@@ -76,7 +74,7 @@ app.use(bodyParser.json({limit: '5mb'}));
  */
 
 // This middleware manages the number of connections, and logs about it.
-app.use('/render', (req, res, next) => {
+app.use("/render", (req, res, next) => {
     // The number of concurrent requests will fluctuate as this
     // request is evaluated.  We arbitrarily take the number at
     // our-request-start as the value we log.
@@ -96,9 +94,13 @@ app.use('/render', (req, res, next) => {
     const end = res.end;
     res.end = (chunk, encoding) => {
         pendingRenderRequests--;
-        if (res.statusCode < 300) {   // only log on successful fetches
-            renderProfile.end(`render-stats for ${req.body.urls[req.body.urls.length - 1]}: ` +
-                              JSON.stringify(req.requestStats));
+        if (res.statusCode < 300) {
+            // only log on successful fetches
+            renderProfile.end(
+                `render-stats for ${
+                    req.body.urls[req.body.urls.length - 1]
+                }: ` + JSON.stringify(req.requestStats),
+            );
         }
         res.end = end;
         res.end(chunk, encoding);
@@ -118,85 +120,103 @@ const checkSecret = function(req, res, next) {
 const handleFetchError = function(err, res) {
     // Error handler for fetching failures.
     if (err.response && err.response.error) {
-        logging.error('Fetching failure: ' + err.response.error + ': ',
-                        err.stack);
+        logging.error(
+            "Fetching failure: " + err.response.error + ": ",
+            err.stack,
+        );
         res.status(500).json({error: err, stack: err.stack});
-    } else if (err.error) {        // set for timeouts, in particular
+    } else if (err.error) {
+        // set for timeouts, in particular
         logging.error(err.error);
         res.status(500).json(err);
     } else {
-        logging.error('Fetching failure: ', err.stack);
-        res.status(500).json({error: err.toString(),
-                                stack: err.stack});
+        logging.error("Fetching failure: ", err.stack);
+        res.status(500).json({error: err.toString(), stack: err.stack});
     }
     // If the error was a timeout, log that fact to graphite.
     if (err.timeout) {
         graphiteUtil.log("react_render_server.stats.timeout", 1);
     }
-}
+};
 
-app.post('/render', checkSecret, (req, res) => {
+const respond400Error = (res, error, value) => {
+    return res.status(400).json({error, value});
+};
+
+app.post("/render", checkSecret, (req, res) => {
     // Validate the input.
-    let err;
-    let value;
+    const {urls, props, globals} = req.body;
 
-    if (
-        !Array.isArray(req.body.urls) ||
-        req.body.urls.length === 0 ||
-        !req.body.urls.every(e => typeof e === 'string') ||
-        !req.body.urls.every(e => e.indexOf('http') === 0)
-    ) {
-            err = ('Missing "urls" keyword in POST JSON input, ' +
-                   'or "urls" is not a list of full urls');
-            value = req.body.urls;
-    } else if (
-        typeof req.body.props !== 'object' ||
-        Array.isArray(req.body.props)
-    ) {
-        err = ('Missing "props" keyword in POST JSON input, ' +
-               'or "props" is not an object, or it has non-string keys.');
-        value = req.body.props;
+    if (!Array.isArray(urls) || !urls.every(url => typeof url === "string")) {
+        return respond400Error(
+            res,
+            'Missing "urls" keyword in POST JSON input, ' +
+                'or "urls" is not a list of strings',
+            urls,
+        );
+    } else if (typeof props !== "object" || Array.isArray(props)) {
+        return respond400Error(
+            res,
+            'Missing "props" keyword in POST JSON input, ' +
+                'or "props" is not an object, or it has non-string keys.',
+            props,
+        );
     }
-    if (err) {
-        res.status(400).json({error: err, value: value});
-        return;
+
+    // We filter out all non-JS URLs as we don't need to download them in
+    // order to render the page (for example .css files may be specified and
+    // we want to ignore them)
+    const jsUrls = urls.filter(
+        url => url.startsWith("http") && url.endsWith(".js"),
+    );
+
+    if (jsUrls.length === 0) {
+        return respond400Error(
+            res,
+            'Error in "urls" keyword in POST JSON input, ' +
+                "no valid JS urls were specified.",
+            urls,
+        );
     }
 
     // Fetch the entry point and its dependencies.
-    const fetchPromises = req.body.urls.map(
-        url => fetchPackage(url, undefined, req.requestStats).then(
-            content => ({url, content}),
-        ),
+    const fetchPromises = jsUrls.map(url =>
+        fetchPackage(url, undefined, req.requestStats).then(content => ({
+            url,
+            content,
+        })),
     );
 
     // TODO(joshuan): Consider moving to async/await.
-    Promise.all(fetchPromises).then(
-        (packages) => render(
-            packages,
-            req.body.props,
-            req.body.globals,
-            undefined,
-            req.requestStats
-        ).then(renderedState => {
-            // We store the updated request-stats in renderedState
-            // (the only way to get the updated data back from our
-            // subprocess); pop that out into update req.requestStats.
-            req.requestStats = renderedState.requestStats;
-            delete renderedState.requestStats;
-            res.json(renderedState);
-        }),
-        err => handleFetchError(err, res),
-    ).catch(err => {
-        logging.error(
-            'Rendering failure: ' + req.body.urls[req.body.urls.length - 1] + ' :',
-            err.stack,
-        );
-        // A rendering error is probably a bad component, so we
-        // give a 400.
-        res.status(400).json({error: err.toString(), stack: err.stack});
-    });
+    Promise.all(fetchPromises)
+        .then(
+            packages =>
+                render(
+                    packages,
+                    props,
+                    globals,
+                    undefined,
+                    req.requestStats,
+                ).then(renderedState => {
+                    // We store the updated request-stats in renderedState
+                    // (the only way to get the updated data back from our
+                    // subprocess); pop that out into update req.requestStats.
+                    req.requestStats = renderedState.requestStats;
+                    delete renderedState.requestStats;
+                    res.json(renderedState);
+                }),
+            err => handleFetchError(err, res),
+        )
+        .catch(err => {
+            logging.error(
+                "Rendering failure: " + jsUrls[jsUrls.length - 1] + " :",
+                err.stack,
+            );
+            // A rendering error is probably a bad component, so we
+            // give a 400.
+            res.status(400).json({error: err.toString(), stack: err.stack});
+        });
 });
-
 
 /**
  * Flush all the caches.
@@ -216,32 +236,32 @@ app.post('/render', checkSecret, (req, res) => {
  * We respond with the instance that was flushed.
  * TODO(csilvers): how do we flush *all* the instances??
  */
-app.post('/flush', checkSecret, (req, res) => {
+app.post("/flush", checkSecret, (req, res) => {
     cache.reset();
-    res.send((process.env['GAE_INSTANCE'] || 'dev') + '\n');
+    res.send((process.env["GAE_INSTANCE"] || "dev") + "\n");
 });
 
-app.get('/_api/ping', (req, res) => res.send('pong!\n'));
+app.get("/_api/ping", (req, res) => res.send("pong!\n"));
 
-app.get('/_api/version', (req, res) => {
+app.get("/_api/version", (req, res) => {
     // This will return the module version ID we set when deploying.
-    res.send((process.env['GAE_VERSION'] || 'dev') + '\n');
+    res.send((process.env["GAE_VERSION"] || "dev") + "\n");
 });
 
 // These are used by the Managed VM lifecycle functions:
 // https://cloud.google.com/appengine/docs/managed-vms/custom-runtimes#lifecycle_events
-app.get('/_ah/health', (req, res) => res.send('ok!\n'));
-app.get('/_ah/start', (req, res) => res.send('ok!\n'));
-app.get('/_ah/stop', (req, res) => res.send('ok!\n'));
-
+app.get("/_ah/health", (req, res) => res.send("ok!\n"));
+app.get("/_ah/start", (req, res) => res.send("ok!\n"));
+app.get("/_ah/stop", (req, res) => res.send("ok!\n"));
 
 // Simplistic priming endpoint. Calling this endpoint uses CPU and thus
 // hopefully causes the autoscaler to spin up more instances. This endpoint
 // takes about 2 seconds when called locally on my laptop.
-app.get('/prime', (req, res) => {
-    for (var i = 0; i < 3000000000; i++) { // eslint-disable-line
+app.get("/prime", (req, res) => {
+    for (let i = 0; i < 3000000000; i++) {
+        // noop
     }
-    res.send('ok\n');
+    res.send("ok\n");
 });
 
 module.exports = app;
