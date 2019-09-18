0864f9beb6c59710688057c24166f9c266561b7a
Jeff Yates <jeff@somewhatabstract.com>
2019-09-18 11:31:21 -0500

    Update fetch_package to pass eslint and use async/await

--- src/fetch_package.js
+++ src/fetch_package.js
@@ -1,5 +1,3 @@
-/* eslint-disable */
-/** TODO(jeff): Re-enable eslint and fix issues once PR #17 lands */
 /**
  * Fetch a package from the place that has packages.
  *
@@ -32,7 +30,7 @@ const inFlightRequests = {};
  * @returns {Promise<{content: string, url: string}>} A promise of an object
  * containing the content and the url from which it came.
  */
-const fetchPackage = function(url, requestStats, triesLeftAfterThisOne) {
+const fetchPackage = async function(url, requestStats, triesLeftAfterThisOne) {
     if (triesLeftAfterThisOne == null) {
         triesLeftAfterThisOne = numRetries;
     }
@@ -55,77 +53,70 @@ const fetchPackage = function(url, requestStats, triesLeftAfterThisOne) {
         );
     };
 
-    const fetchPromise = new Promise((resolve, reject) => {
+    const doFetch = async () => {
         // Now create the request.
         const fetcher = request.get(url);
 
         // We give the fetcher 60 seconds to get a response.
         fetcher.timeout(60000);
 
-        // Now we handle when the request ends, either successfully or
-        // otherwise.
-        fetcher.buffer().end((err, res) => {
-            // The request is done: don't say it's inflight anymore!
+        let success = false;
+        try {
+            // Now we handle when the request ends, either successfully or
+            // otherwise.
+            const result = await fetcher.buffer();
+            success = true;
+
             // (Note: when running tests, our key may not be in
             // inFlightRequests, if this promise resolves after the
             // termination of the test.  In that case, we can just bail,
             // since the test isn't running anymore anyway.)
             if (!inFlightRequests[url]) {
-                reject("We've moved on to other tests, my friend");
-                return;
+                throw new Error("We've moved on to other tests, my friend");
             }
+
+            if (requestStats) {
+                requestStats.packageFetches++;
+            }
+
+            return {
+                content: result.text,
+                url,
+            };
+        } finally {
+            reportFetchTime(success);
+            // The request is done: don't say it's inflight anymore!
             delete inFlightRequests[url];
+        }
+    };
 
-            if (err) {
-                if (
-                    err.response &&
-                    err.response.status >= 400 &&
-                    err.response.status < 500
-                ) {
-                    // One could imagine adding a 'negative' cache
-                    // entry for 4xx errors, maybe with a maxAge of 1
-                    // minute, but unless we see this being a problem
-                    // in practice it's not worth the code complexity.
-                    reject(err);
-                    return;
-                }
-                // If we get here, we have a 5xx error or similar
-                // (socket timeout, maybe).  Let's retry a few times.
-                if (triesLeftAfterThisOne > 0) {
-                    fetchPackage(
-                        url,
-                        requestStats,
-                        triesLeftAfterThisOne - 1,
-                    ).then(resolve, reject);
-                    return;
-                }
+    const fetchPromise = doFetch().catch((err) => {
+        if (
+            err.response &&
+            err.response.status >= 400 &&
+            err.response.status < 500
+        ) {
+            // One could imagine adding a 'negative' cache
+            // entry for 4xx errors, maybe with a maxAge of 1
+            // minute, but unless we see this being a problem
+            // in practice it's not worth the code complexity.
+            throw err;
+        }
 
-                // OK, I give up.
-                reject(err);
-            } else {
-                if (requestStats) {
-                    requestStats.packageFetches++;
-                }
-                resolve({
-                    content: res.text,
-                    url,
-                });
-            }
-        });
+        // If we get here, we have a 5xx error or similar
+        // (socket timeout, maybe).  Let's retry a few times.
+        if (triesLeftAfterThisOne > 0) {
+            return fetchPackage(url, requestStats, triesLeftAfterThisOne - 1);
+        }
+
+        // OK, I give up.
+        throw err;
     });
-
-    // Terminate the profiling.
-    fetchPromise.then(
-        () => reportFetchTime(true),
-        () => reportFetchTime(false),
-    );
-
-    const retval = fetchPromise;
 
     // Let other concurrent requests know that we're fetching this
     // url, so they don't try to do it too.
-    inFlightRequests[url] = retval;
-    return retval;
+    inFlightRequests[url] = fetchPromise;
+    return fetchPromise;
 };
 
 module.exports = fetchPackage;
