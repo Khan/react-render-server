5ad0beb05e0691f35bd4ed067efde63d911e2055
Jeff Yates <jeff@somewhatabstract.com>
2019-09-26 18:39:21 -0500

    Make sure app.js is covered by linters; just to be safe - even if we suppress some things

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
+// @noflow
 // This is only needed when deploying to Appengine Standard
 // (app.js is the hard-coded entrypoint for appengine-standard apps).
-// eslint-disable-next-line import/no-unassigned-import
+// eslint-disable-next-line import/no-unassigned-import, import/no-commonjs
 require("./dist/main.js");
