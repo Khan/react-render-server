6020d69084256e6083d8f8e6a78a0b023dde1e50
Jeff Yates <jeff@somewhatabstract.com>
2020-02-06 00:45:19 -0600

    Make sure it actually works

--- src/trace-agent.js
+++ src/trace-agent.js
@@ -2,7 +2,7 @@
 import args from "./arguments.js";
 
 // Start logging agent for Cloud Trace (https://cloud.google.com/trace/).
-import traceAgent from "@google-cloud/trace-agent";
+import * as traceAgent from "@google-cloud/trace-agent";
 
 export const tracer = traceAgent.start({
     enabled: !args.dev,
