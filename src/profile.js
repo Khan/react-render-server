d9cef7fd2f18f36995daf84342cfdada2ca2229c
Jeff Yates <jeff@somewhatabstract.com>
2020-02-06 14:08:48 -0600

    Add PROFILE: to start of trace spans
    This is just to add some clarity in the trace as to what was created by this versus some other code

--- src/profile.js
+++ src/profile.js
@@ -42,7 +42,13 @@ const start = (msg: string): ProfileSession => {
     // summary. However the start markers may be useful if we have to dig in.
     logging.silly(`PROFILE(start): ${msg}`);
 
-    const span = tracer.createChildSpan({name: msg});
+    /**
+     * Start a trace section for this so it will appear in Stackdriver Trace
+     * We annotate the start with "PROFILE:" so that it is clear in the trace
+     * which spans were created by this API and which were inserted by other
+     * means.
+     */
+    const span = tracer.createChildSpan({name: `PROFILE: ${msg}`});
     const profiler = logging.startTimer();
     return {
         end: (endMsg?: string, level?: LogLevel = "debug") => {
